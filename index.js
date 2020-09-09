18a30602bc15483167448fd5f87976af768b1f38
Fenixor <71027299+Fenixor@users.noreply.github.com>
2020-09-09 18:20:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ bot.on('ready', function () {
     bot.user.setActivity('rien').catch(console.error)
 });
 
+bot.on('message', msg => {
+    if (msg.content === "bonjour"){
+        msg.reply("Heureux de te revoir parmis nous.")
+    }
+    if (msg.content.match(/salut/i)) {
+            msg.reply('Je suis d\'accord avec toi.')
+    }
+    if (msg.content === prefix + "site"){
+        msg.channel.send("https://alexpgm.000webhostapp.com/")
+        console.log("Une personne a demandé pour aller sur ton site.")
+    }
+
+});
+
 
 
 bot.login(token); 
