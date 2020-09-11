62d7680d7ed081cb5c85389a6dcfcc90dac3242d
Fenixor <71027299+Fenixor@users.noreply.github.com>
2020-09-11 19:34:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ msg.awaitReactions(filter, { max: 1, time: 60000})
 		const reaction = collected.first();
 
 		if (reaction.emoji.name === '👍') {
-			client.message.send(author, "pong");
+			client.msg.send(author, "pong");
 		} else {
 			msg.reply('you reacted with a thumbs down.');
 		}
