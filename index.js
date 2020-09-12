f6e2d6722ca8439abbb305d5df5855e2788a0d1f
Fenixor <71027299+Fenixor@users.noreply.github.com>
2020-09-12 12:32:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,46 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = process.env.token;
 
+
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
     bot.user.setActivity('être testé par Fenixor').catch(console.error)
 	
     });
+
+client.on("message", message => {
+	
+	if (msg.content === "bonjour"){
+        msg.channel.send('My Message')
+		.then(msg => {msg.react('🎉')
+			     }})
+	
+	
+		
+		
+		
+		      
+			      
+			      
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+})
+
+
+
+
+
+
+
 
 
 
