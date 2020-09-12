0544f1081ce8b58f431d5e3ea52e866b772408ed
Fenixor <71027299+Fenixor@users.noreply.github.com>
2020-09-12 12:06:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const cfg = require('./index.json');
 const token = process.env.token;
-const prefix = (cfg.prefix);
+
+
+
+
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
     bot.user.setActivity('être testé par Fenixor').catch(console.error)
 	
-    
-});
+    });
 
 bot.on('message', msg => {
 	const filter = (reaction, user) => {
@@ -17,25 +18,12 @@ bot.on('message', msg => {
     if (msg.content === "bonjour"){
         msg.channel.send('My Message')
 	    .then(msg => {msg.react('🎉')
-  });
+  }});
   
-    }
-    if (msg.content.match(/salut/i)) {
-            msg.reply('Je suis d\'accord avec toi.')
-    }
-    if (msg.content === prefix + "site"){
-        msg.channel.send("https://alexpgm.000webhostapp.com/")
-        console.log("Une personne a demandé pour aller sur ton site.")
-    }
-    if (msg.content.match(/!Fenixor/i)) {
-            msg.reply('Fenixor est mon dévelopeur.')
-    }
-    if (msg.content.match(/!Aide/i)) {
-            msg.reply('!bonjour **\n !salut')
-    }
-	if (msg.content === '!react') {
-	msg.react('😄');
-	}
+   
+ 
+	
+	
 
     
 	
