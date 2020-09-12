5655af9474584f1c5ef8533c3ba2a88b15e73a9f
Fenixor <71027299+Fenixor@users.noreply.github.com>
2020-09-12 12:14:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,9 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-const token = process.env.token;
+const client = new Discord.Client();
+const token = ‘INSEREZ VOTRE TOKEN ICI’
 
+client.once('ready', () => {
+   console.log(‘Félicitations, votre bot Discord a été correctement initialisé !');
+});
 
-
-
-bot.on('ready', function () {
-    console.log("Je suis prêt à être utilisé.")
-    bot.user.setActivity('être testé par Fenixor').catch(console.error)
-	
-    });
-
-bot.on('message', msg => {
-	const filter = (reaction, user) => {
-	return ['👍', '👎'].includes(reaction.emoji.name) && user.id === msg.author.id;
-};
-    if (msg.content === "bonjour"){
-        msg.channel.send('My Message')
-	    .then(msg => {msg.react('🎉')
-			 
-		  
-  });
-  
-   
- 
-	
-	
-
-    
-	
-msg.awaitReactions(filter, { max: 1, time: 60000})
-	.then(collected => {
-		const reaction = collected.first();
-
-		if (reaction.emoji.name === '👍') {
-			bot.msg.send("pong");
-		} else {
-			msg.reply('you reacted with a thumbs down.');
-		}
-
-});	
-});	
-
-
-
-       
-
-bot.login(token); 
+client.login(token);
