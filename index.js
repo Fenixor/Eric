2e8a3479a582c9836fe746f67ec7cf413c8612a9
Fenixor <71027299+Fenixor@users.noreply.github.com>
2020-09-12 12:23:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = ‘NzUzMjY1MjMzNTQzNTYxMjQ5.X1jq6g.P3V7_8eUVba-s6l-K3pbdDtN09w’
+const token = process.env.token;
 
 client.once('ready', () => {
    console.log(‘Félicitations, votre bot Discord a été correctement initialisé !');
